ff7ac7d713885d77f03deb98d340abdc066418f8
Shadasviar <shadasviarus@gmail.com>
2018-03-24 18:54:11 +0100

    Add license

--- cpp/histogram.cpp
+++ cpp/histogram.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of HarbuzHIST18. 
+ * histogram.cpp
+ * Copyright (C) Uladzislau Harbuz 2018 
+ * 
+ * HarbuzHIST18 is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * HarbuzHIST18 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "histogram.h"
 #include "ui_histogram.h"
 #include <utility>
